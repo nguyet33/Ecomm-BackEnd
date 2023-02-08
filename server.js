1bb5d61f75023f717c58894aff9c56b3e53ec816
nguyet33 <tdnguyet33@gmail.com>
2023-02-07 22:49:59 -0800

    finished model/routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync Sequelize models to the database, then turn on the server
+sequelize.sync({force: true}).then(function() {
+  app.listen(PORT, function() {
+    console.log('App listening on PORT' + PORT);
+  });
+});
+
